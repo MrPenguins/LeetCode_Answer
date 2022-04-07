7ce44089a6b61a3e68801bc272e5bcff5670d6a7
MrPenguins <zhanghaoqi321@gmail.com>
2022-04-07 18:42:57 +0800

    LeetCode 35. 搜索插入位置

---
+++ LeetCode/35. 搜索插入位置/Solution1.java
@@ -0,0 +1,18 @@
+// 本题可理解为找到第一个大于等于target的数字的下标
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
