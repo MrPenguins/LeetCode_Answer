709bdda14b89d486b54cc80818d6400c1c98d2f0
MrPenguins <zhanghaoqi321@gmail.com>
2022-08-22 21:42:30 +0800

    LeetCode 509. 斐波那契数

---
+++ LeetCode/509. 斐波那契数/Solution1.java
@@ -0,0 +1,18 @@
+// 时间复杂度O(n)
+// 空间复杂度O(1)
+
+class Solution {
+    public int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
