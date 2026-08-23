40dde9a109ea2a7558b4291621bdd2bbbd8b8ca4
MrPenguins <56572306+MrPenguins@users.noreply.github.com>
2026-08-23 16:06:37 +0300

    142. 环形链表 II

---
+++ LeetCode_python/142. 环形链表 II.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = slow = head
+        flag = False
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                flag = True
+                break
+        if not flag:
+            return None
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
