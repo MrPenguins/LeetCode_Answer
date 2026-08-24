8f00dab305c5db9aa69f266977468495a1640353
MrPenguins <56572306+MrPenguins@users.noreply.github.com>
2026-08-24 09:24:42 +0300

    24. 两两交换链表中的节点

---
+++ LeetCode_python/24. 两两交换链表中的节点.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        cur = dummy
+        while cur.next and cur.next.next:
+            first = cur.next
+            second = cur.next.next
+            cur.next = second
+            first.next = second.next
+            second.next = first
+            cur = first
+        return dummy.next
